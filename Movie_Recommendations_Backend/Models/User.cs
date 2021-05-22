42542cadc87d73da9f0f43312bf0827bedd525da
Alexandru Lupusoru <alexandru.lupusoru@student.tuiasi.ro>
2021-05-22 19:20:48 +0300

    added model, view and controller

---
+++ Movie_Recommendations_Backend/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Movie_Recommendations_Backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime BirthDay { get; set; }
+    }
+}
