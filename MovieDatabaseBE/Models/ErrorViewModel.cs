71e960591f06f833b99f4bdfd78833699835dc0e
Alexandru Lupusoru <alexandru.lupusoru@student.tuiasi.ro>
2021-05-22 19:23:26 +0300

    added model, controller and view

---
+++ MovieDatabaseBE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieDatabaseBE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
